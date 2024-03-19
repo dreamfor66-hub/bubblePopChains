378784851a9d9ec6ec8504242f6ea82f1d9d7498
dreamfor66-hub <dreamfor66@gmail.com>
2024-03-20 03:10:45 +0900

    몬스터 스폰

--- Assets/MonsterSpawner.cs
+++ Assets/MonsterSpawner.cs
@@ -19,33 +19,37 @@ public class MonsterSpawner : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         stageManager = GetComponentInParent<StageManager>();
-        pool = FindObjectOfType<CellPool>().transform;
+        pool = FindObjectOfType<MonsterContainer>().transform;
+        StartCoroutine(RandomSpawn());
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        var hits = Physics2D.BoxCastAll(this.transform.position, Vector2.one, 0f, Vector2.up, 0.01f);
-        if (!hits.Any(x => x.collider.gameObject.name.Contains("Cell")))
-        {
-            RandomSpawn();
-        }
 
     }
-    public void RandomSpawn()
+
+    public IEnumerator RandomSpawn()
     {
-        var rand = Random.Range(0, 1);
-        var isMinus = Random.Range(0, 2); //1이면 양수 0이면 음수
-        float randPos = (float)Random.Range(1, 100) / 100; //Tofloat
-        if (isMinus == 0)
+        var time = 999f;
+        while (time > 0)
         {
-            randPos *= 1;
+
+            var rand = Random.Range(0, 1);
+            var isMinus = Random.Range(0, 2); //1이면 양수 0이면 음수
+            float randPos = (float)Random.Range(1, 100) / 20; //Tofloat
+            if (isMinus == 0)
+            {
+                randPos *= 1;
+            }
+            else if (isMinus == 1)
+            {
+                randPos *= -1;
+            }
+            Instantiate(monsters[rand], new Vector2(transform.position.x + randPos, transform.position.y), Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))), pool);
+            yield return new WaitForSeconds(1f);
         }
-        else if (isMinus == 1)
-        {
-            randPos *= -1;
-        }
-        Instantiate(monsters[rand], new Vector2(transform.position.x + randPos, transform.position.y - .5f), Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))), pool);
+        
         //cellPool.CellContainer.Add(cells[rand]);
     }
 
